e696d45f12dcc52cac1cd9f132d75927a9d8db14
Long Lam <lamhoanglong.3001@gmail.com>
2024-02-05 22:18:30 -0500

    feat: add and test get project endpoints

--- packages/api-gateway/test/app.e2e-spec.ts
+++ packages/api-gateway/test/app.e2e-spec.ts
@@ -45,13 +45,13 @@ beforeEach(async () => {
 // describe('Project Creation Routes', () => {});
 describe('Auth Routes', () => {
   it('Returns empty value', async () => {
-    request(app.getHttpServer()).get('/auth').expect(200).expect('');
+    await request(app.getHttpServer()).get('/auth').expect(200).expect('');
   });
 });
 
 describe('Project Creation Routes', () => {
   it('Create a project with valid inputs', async () => {
-    return await request(app.getHttpServer())
+    await request(app.getHttpServer())
       .post('/project')
       .send({
         name: 'testProject',
@@ -61,7 +61,7 @@ describe('Project Creation Routes', () => {
   });
 
   it("Create a project with empty-string name", async () => {
-    return await request(app.getHttpServer())
+    await request(app.getHttpServer())
       .post('/project')
       .send({
         name: '',
@@ -71,7 +71,7 @@ describe('Project Creation Routes', () => {
   });
 
   it("Create a project with no name field", async () => {
-    return await request(app.getHttpServer())
+    await request(app.getHttpServer())
       .post('/project')
       .send({
       })
@@ -80,7 +80,7 @@ describe('Project Creation Routes', () => {
   });
 
   it("Create a project with null name", async () => {
-    return await request(app.getHttpServer())
+    await request(app.getHttpServer())
       .post('/project')
       .send({
         name: null,
@@ -90,7 +90,7 @@ describe('Project Creation Routes', () => {
   });
 
   it("Create a project with non-string name", async () => {
-    return await request(app.getHttpServer())
+    await request(app.getHttpServer())
       .post('/project')
       .send({
         name: 1,
@@ -98,4 +98,47 @@ describe('Project Creation Routes', () => {
       .expect(201)
       .expect("");
   });
+});
+
+describe('Project Retrieval Routes', () => {
+  it('Get project with valid id', async () => {
+    await request(app.getHttpServer())
+      .get('/project/id/1')
+      .expect(200)
+      .then((response) => {
+        expect(response.body.name).toEqual('testProject');
+        expect(response.body.id).toEqual(1);
+      })
+  });
+
+  it('Get project with valid name', async () => {
+    await request(app.getHttpServer())
+      .get('/project/name/testProject')
+      .expect(200)
+      .then((response) => {
+        expect(response.body.name).toEqual('testProject');
+        expect(response.body.id).toEqual(1);
+      })
+  });
+
+  it('Get project with non-number id', async () => {
+    await request(app.getHttpServer())
+      .get('/project/id/abc')
+      .expect(400)
+      .expect('{"statusCode":400,"message":"id must be a number"}')
+  });
+
+  // it('Get project with non-existent id', async () => {
+  //   await request(app.getHttpServer())
+  //     .get('/project/id/0')
+  //     .expect(400)
+  //     .expect('{"statusCode":400,"message":"id does not exist"}')
+  // });
+
+  // it('Get project with non-existent name', async () => {
+  //   await request(app.getHttpServer())
+  //     .get('/project/name/abc')
+  //     .expect(400)
+  //     .expect('{"statusCode":400,"message":"name does not exist"}')
+  // });
 });
