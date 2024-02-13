d6e72b0d2fc48376a6233ac5f21f6f5579943a88
Hayden Carpenter <haydencarp@outlook.com>
2024-02-13 14:10:04 -0500

    Fix recovery

--- packages/db-service/src/modules/auth/auth.controller.ts
+++ packages/db-service/src/modules/auth/auth.controller.ts
@@ -2,16 +2,13 @@ import { Controller } from '@nestjs/common';
 import { ApiKey } from '@prisma/client';
 import { AuthService } from './auth.service';
 import { ApiKeyProto } from 'juno-proto';
-import { Observable } from 'rxjs';
 
 @Controller()
 @ApiKeyProto.ApiKeyServiceControllerMethods()
 export class ApiKeyController implements ApiKeyProto.ApiKeyServiceController {
   constructor(private readonly apiKeyService: AuthService) {}
 
-  async createApiKey(
-    request: ApiKeyProto.CreateApiKeyParams,
-  ): Promise<Observable<ApiKey>> {
+  async createApiKey(request: ApiKeyProto.CreateApiKeyParams): Promise<ApiKey> {
     // const apiKey = await this.apiKeyService.createApiKey(request.apiKey);
     const apiKey = this.apiKeyService.createApiKey(request);
     return apiKey;
