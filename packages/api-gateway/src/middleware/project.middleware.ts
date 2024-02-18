c67493c372e5af9e87b8ef57781ea3e133af2f53
Long Lam <lamhoanglong.3001@gmail.com>
2024-02-17 22:31:46 -0500

    feat: add jwt validation to middleware

--- packages/api-gateway/src/middleware/project.middleware.ts
+++ packages/api-gateway/src/middleware/project.middleware.ts
@@ -1,10 +1,53 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  Injectable,
+  NestMiddleware,
+  Inject,
+  OnModuleInit,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { ClientGrpc } from '@nestjs/microservices';
+import { JwtProto } from 'juno-proto';
+import { lastValueFrom } from 'rxjs';
+
+const { JWT_SERVICE_NAME } = JwtProto;
 
 @Injectable()
-export class ProjectLinkingMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
+export class ProjectLinkingMiddleware implements NestMiddleware, OnModuleInit {
+  private jwtService: JwtProto.JwtServiceClient;
+
+  constructor(@Inject(JWT_SERVICE_NAME) private jwtClient: ClientGrpc) {}
+
+  onModuleInit() {
+    this.jwtService = this.jwtClient.getService<JwtProto.JwtServiceClient>(
+      JwtProto.JWT_SERVICE_NAME,
+    );
+  }
+
+  async use(req: Request, res: Response, next: NextFunction) {
     console.log('Middleware: ' + req.originalUrl);
-    next();
+    try {
+      if (!req.headers.authorization) {
+        throw new Error('No authorization headers');
+      }
+      const token = this.extractTokenFromHeader(req);
+      if (!token) {
+        throw new Error('Jwt not found');
+      }
+      const jwtValidation = this.jwtService.validateJwt({ jwt: token });
+      await lastValueFrom(jwtValidation);
+      next();
+    } catch {
+      throw new HttpException(
+        'Invalid user credentials',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
--- packages/api-gateway/src/modules/user/user.module.ts
+++ packages/api-gateway/src/modules/user/user.module.ts
@@ -9,9 +9,10 @@ import { ConfigModule } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { UserController } from './user.controller';
-import { UserProto, UserProtoFile } from 'juno-proto';
+import { UserProto, UserProtoFile, JwtProto, JwtProtoFile } from 'juno-proto';
 
 const { USER_SERVICE_NAME, JUNO_USER_PACKAGE_NAME } = UserProto;
+const { JWT_SERVICE_NAME, JUNO_JWT_PACKAGE_NAME } = JwtProto;
 
 @Module({
   imports: [
@@ -19,6 +20,15 @@ const { USER_SERVICE_NAME, JUNO_USER_PACKAGE_NAME } = UserProto;
       envFilePath: join(__dirname, '../../../../../.env.local'),
     }),
     ClientsModule.register([
+      {
+        name: JWT_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: process.env.AUTH_SERVICE_ADDR,
+          package: JUNO_JWT_PACKAGE_NAME,
+          protoPath: JwtProtoFile,
+        },
+      },
       {
         name: USER_SERVICE_NAME,
         transport: Transport.GRPC,
