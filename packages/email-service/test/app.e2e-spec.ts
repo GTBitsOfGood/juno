96cac880facb282ad9a90df7dc918aa4f9c3ac95
Tejas Mehta <tmthecoder@gmail.com>
2024-02-22 18:30:43 -0500

    Add email service (#53)

---
+++ packages/email-service/test/app.e2e-spec.ts
@@ -0,0 +1,58 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestMicroservice } from '@nestjs/common';
+import { AppModule } from './../src/app.module';
+import * as ProtoLoader from '@grpc/proto-loader';
+import * as GRPC from '@grpc/grpc-js';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ResetProtoFile } from 'juno-proto';
+
+let app: INestMicroservice;
+
+jest.setTimeout(10000);
+
+async function initApp() {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
+
+  const app = moduleFixture.createNestMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: [],
+      protoPath: [],
+      url: process.env.EMAIL_SERVICE_ADDR,
+    },
+  });
+
+  await app.init();
+
+  await app.listen();
+  return app;
+}
+
+beforeAll(async () => {
+  app = await initApp();
+
+  const proto = ProtoLoader.loadSync([ResetProtoFile]) as any;
+
+  const protoGRPC = GRPC.loadPackageDefinition(proto) as any;
+
+  const resetClient = new protoGRPC.juno.reset_db.DatabaseReset(
+    process.env.DB_SERVICE_ADDR,
+    GRPC.credentials.createInsecure(),
+  );
+
+  await new Promise((resolve) => {
+    resetClient.resetDb({}, () => {
+      resolve(0);
+    });
+  });
+});
+
+afterAll(() => {
+  app.close();
+});
+
+it('Dummy test', () => {
+  expect('1').toEqual('1');
+});
