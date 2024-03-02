e48b83f545da0d926d1f73bb1c8b90fbe4ec55bd
jpise3 <63403110+jaipise@users.noreply.github.com>
2024-03-02 01:01:57 -0500

    Completed implemementation of sendgrid sender registration

--- packages/email-service/src/app.controller.ts
+++ packages/email-service/src/app.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Post, Body, Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EmailProto } from 'juno-proto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Post('/register-sender')
+  async registerSender(
+    @Body() req: EmailProto.RegisterSenderRequest,
+  ): Promise<EmailProto.RegisterSenderResponse> {
+    return await this.appService.registerSender(req);
+  }
 }
