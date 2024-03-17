acdd9391036e65ead7e3d5da834c43508f5cf5e2
Tejas Mehta <tmthecoder@gmail.com>
2024-03-17 00:49:43 -0400

    Add logging service base

---
+++ packages/logging-service/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+
+async function bootstrap() {
+  ConfigModule.forRoot({
+    envFilePath: join(__dirname, '../../../.env.local'),
+  });
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: [],
+        protoPath: [],
+        url: process.env.LOGGING_SERVICE_ADDR,
+      },
+    },
+  );
+  await app.listen();
+}
+bootstrap();
