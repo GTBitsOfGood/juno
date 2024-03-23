cae9f74073f78045fb3be25d2315f5fbb3c15e85
jpise3 <63403110+jaipise@users.noreply.github.com>
2024-03-22 20:31:21 -0400

    Create the basic service and foundational functionality

--- packages/logging-service/src/app.module.ts
+++ packages/logging-service/src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggingModule } from './modules/logging/logging.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import path from 'path';
 
 @Module({
-  imports: [LoggingModule],
+  imports: [
+    LoggingModule,
+    ClientsModule.register([
+      {
+        name: 'LOGGING_SERVICE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'error_logging',
+          protoPath: path.join(
+            __dirname,
+            '../../',
+            'juno-proto/dist/definitions',
+            'error_loggin.proto',
+          ),
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
