5a19b74189ac1fbdaded240c64edc891186b0bc6
jpise3 <63403110+jaipise@users.noreply.github.com>
2024-03-22 20:50:55 -0400

    fix path import

--- packages/logging-service/src/app.module.ts
+++ packages/logging-service/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import path from 'path';
+import * as path from 'path';
 
 @Module({
   imports: [
