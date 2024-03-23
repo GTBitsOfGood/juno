26120de83bccc8a247ccbfe1cf284afb497a4e70
jpise3 <63403110+jaipise@users.noreply.github.com>
2024-03-22 20:54:03 -0400

    fix typo

--- packages/logging-service/src/app.module.ts
+++ packages/logging-service/src/app.module.ts
@@ -16,7 +16,7 @@ import * as path from 'path';
             __dirname,
             '../../',
             'juno-proto/dist/definitions',
-            'error_loggin.proto',
+            'error_logging.proto',
           ),
         },
       },
