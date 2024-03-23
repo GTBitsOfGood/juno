ea2c0fad3db626b48f1d2a59eb588fcdd9b96b8f
jpise3 <63403110+jaipise@users.noreply.github.com>
2024-03-22 20:31:21 -0400

    Create the basic service and foundational functionality

--- packages/logging-service/src/app.service.ts
+++ packages/logging-service/src/app.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+
+interface LoggingService {
+    recordError(errorLogRequest: { message: string }): Observable<void>;
+}
 
 @Injectable()
-export class AppService {}
+export class AppService implements OnModuleInit {
+    private loggingService: LoggingService;
+
+    constructor(@Inject('LOGGING_SERVICE') private client: ClientGrpc) {}
+
+    onModuleInit() {
+        this.loggingService = this.client.getService<LoggingService>('LoggingService');
+    }
+
+    recordError(message: string) {
+        // TODO: currently doing nothing here, but want to actually log
+        return;
+    }
+}
