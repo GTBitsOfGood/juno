6eaa84ad082ab3f5d534826494fb707fc10a3639
Tejas Mehta <tmthecoder@gmail.com>
2024-03-26 14:09:55 -0400

    Add openapi base

--- packages/api-gateway/src/main.ts
+++ packages/api-gateway/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { RpcExceptionFilter } from './rpc_exception_filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,14 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new RpcExceptionFilter());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('Juno')
+    .setDescription('Juno Public API Docs')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
