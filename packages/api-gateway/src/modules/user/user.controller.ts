3df0252cb89cea8bd3bd45a943578eda445c8a5b
ahong75 <anthonyhong2003@gmail.com>
2024-03-30 13:28:59 -0400

    added docs for user functions

--- packages/api-gateway/src/modules/user/user.controller.ts
+++ packages/api-gateway/src/modules/user/user.controller.ts
@@ -10,6 +10,13 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import {
+  ApiResponse,
+  ApiTags,
+  ApiParam,
+  ApiBody,
+  ApiOperation,
+} from '@nestjs/swagger';
 import { ClientGrpc } from '@nestjs/microservices';
 import { UserProto } from 'juno-proto';
 import { lastValueFrom } from 'rxjs';
@@ -22,6 +29,7 @@ import {
 
 const { USER_SERVICE_NAME } = UserProto;
 
+@ApiTags('user')
 @Controller('user')
 export class UserController implements OnModuleInit {
   private userService: UserProto.UserServiceClient;
@@ -36,6 +44,26 @@ export class UserController implements OnModuleInit {
   }
 
   @Get('id/:id')
+  @ApiOperation({ summary: 'Get an existing user' })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'The unique identifier of the user',
+    type: String,
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'id must be a number',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The found user',
+    type: UserResponse,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'No user with id found',
+  })
   async getUserById(@Param('id') idStr: string): Promise<UserResponse> {
     const id = parseInt(idStr);
     if (Number.isNaN(id)) {
@@ -49,6 +77,21 @@ export class UserController implements OnModuleInit {
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiBody({ type: CreateUserModel, description: 'The user details' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The user has been successfully created.',
+    type: UserResponse,
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Bad request',
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'Internal server error.',
+  })
   async createUser(@Body() params: CreateUserModel) {
     const user = this.userService.createUser({
       ...params,
@@ -59,6 +102,27 @@ export class UserController implements OnModuleInit {
   }
 
   @Post('type')
+  @ApiOperation({
+    summary: 'Set User Type',
+    description: 'Updates the user type for an existing user.',
+  })
+  @ApiBody({
+    type: SetUserTypeModel,
+    description: 'User ID, email, and the new type to be set',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The updated user',
+    type: UserResponse,
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Bad request',
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'Internal server error.',
+  })
   async setUserType(@Body() setUserTypeParams: SetUserTypeModel) {
     await lastValueFrom(
       this.userService.updateUser({
@@ -74,6 +138,31 @@ export class UserController implements OnModuleInit {
   }
 
   @Put('id/:id/project')
+  @ApiOperation({
+    summary: 'Link User to Project',
+    description: 'Associates a user with a project by their IDs.',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'User ID that is being linked to a project',
+    type: String,
+  })
+  @ApiBody({
+    type: LinkProjectModel,
+    description: 'Project details to link with the user',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Bad request',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'User linked to project successfully.',
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'Internal server error.',
+  })
   async linkUserWithProjectId(
     @Param('id') idStr: string,
     @Body() linkProjectBody: LinkProjectModel,
