64fce822b185e0abb4ecf1029a631639cd2997d5
Tejas Mehta <tmthecoder@gmail.com>
2024-03-30 11:27:26 -0400

    fix service def

--- packages/logging-service/src/app.service.ts
+++ packages/logging-service/src/app.service.ts
@@ -1,25 +1,4 @@
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
-import { ClientGrpc } from '@nestjs/microservices';
-import { Observable } from 'rxjs';
-
-interface LoggingService {
-  recordError(errorLogRequest: { message: string }): Observable<void>;
-}
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService implements OnModuleInit {
-  private loggingService: LoggingService;
-
-  constructor(@Inject('LOGGING_SERVICE') private client: ClientGrpc) {}
-
-  onModuleInit() {
-    this.loggingService =
-      this.client.getService<LoggingService>('LoggingService');
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  recordError(message: string) {
-    // TODO: currently doing nothing here, but want to actually log the error
-    return;
-  }
-}
+export class AppService {}
