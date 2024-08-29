8de9485b1a30f721aff07fe651e9817b69240659
Tejas Mehta <tmthecoder@gmail.com>
2024-08-29 13:31:50 -0400

    Add gateway health check and run yaml getter in release (#87)

---
+++ docker/wait-for-healthy-gateway.sh
@@ -0,0 +1,16 @@
+_wait() {
+    [[ $# -lt 2 ]] && { 
+        echo "Usage: ${FUNCNAME} <timeout> </path/to/script>"; return 1
+    }
+    timeout=$(($(date +%s) + ${1}))
+    until [[ $2 ]] || [[ $(date +%s) -gt $timeout ]]; do
+       :
+    done
+    [[ $(date +%s) -gt $timeout ]] && return 1 || return 0
+}
+
+cmd='[ $(docker ps -f health=healthy -f name=juno-api-gateway -q | wc -l) -eq 1 ]'     # script that only checks health
+_wait 300 $cmd                        # maximum time to wait 300s
+[[ $? -eq 0 ]] && {
+    wget $(docker port juno-api-gateway-1 3000)/docs-yaml
+} || exit 1
