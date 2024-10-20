61e1e4a767cbe300cb2c09f0f1aaa6d713621609
Long Lam <llam36@gatech.edu>
2024-10-20 19:48:18 -0400

    feat: import file proto

--- packages/db-service/src/main.ts
+++ packages/db-service/src/main.ts
@@ -18,6 +18,8 @@ import {
   ApiKeyProtoFile,
   EmailProto,
   EmailProtoFile,
+  FileProto,
+  FileProtoFile,
 } from 'juno-proto';
 import { CustomRpcExceptionFilter } from './global-exception.filter';
 
@@ -37,6 +39,7 @@ async function bootstrap() {
           ResetProto.JUNO_RESET_DB_PACKAGE_NAME,
           ApiKeyProto.JUNO_API_KEY_PACKAGE_NAME,
           EmailProto.JUNO_EMAIL_PACKAGE_NAME,
+          FileProto.JUNO_FILE_SERVICE_FILE_PACKAGE_NAME,
         ],
         protoPath: [
           UserProtoFile,
@@ -46,6 +49,7 @@ async function bootstrap() {
           ResetProtoFile,
           ApiKeyProtoFile,
           EmailProtoFile,
+          FileProtoFile,
         ],
         url: process.env.DB_SERVICE_ADDR,
       },
