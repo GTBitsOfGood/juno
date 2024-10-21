0af8954b8816f2c2be8fc084626e267764183c1a
Long Lam <llam36@gatech.edu>
2024-10-20 20:57:29 -0400

    fix: modify proto type

--- packages/proto/src/gen/file.ts
+++ packages/proto/src/gen/file.ts
@@ -5,45 +5,45 @@
 // source: file.proto
 
 /* eslint-disable */
-import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices';
-import { Observable } from 'rxjs';
+import { GrpcMethod, GrpcStreamMethod } from "@nestjs/microservices";
+import { Observable } from "rxjs";
 
-export const protobufPackage = 'juno.file_service.file';
+export const protobufPackage = "juno.file_service.file";
 
 export interface File {
-  bucket: string;
-  config: string;
+  bucketName: string;
+  configId: number;
   filePath: string;
   metadata: string;
 }
 
 export interface GetFileRequest {
-  bucket: string;
-  config: string;
+  bucketName: string;
+  configId: number;
   filePath: string;
 }
 
 export interface CreateFileRequest {
-  bucket: string;
-  config: string;
+  bucketName: string;
+  configId: number;
   filePath: string;
   metadata: string;
 }
 
 export interface UpdateFileRequest {
-  bucket: string;
-  config: string;
+  bucketName: string;
+  configId: number;
   filePath: string;
   metadata: string;
 }
 
 export interface DeleteFileRequest {
-  bucket: string;
-  config: string;
+  bucketName: string;
+  configId: number;
   filePath: string;
 }
 
-export const JUNO_FILE_SERVICE_FILE_PACKAGE_NAME = 'juno.file_service.file';
+export const JUNO_FILE_SERVICE_FILE_PACKAGE_NAME = "juno.file_service.file";
 
 export interface FileDbServiceClient {
   getFile(request: GetFileRequest): Observable<File>;
@@ -58,51 +58,26 @@ export interface FileDbServiceClient {
 export interface FileDbServiceController {
   getFile(request: GetFileRequest): Promise<File> | Observable<File> | File;
 
-  createFile(
-    request: CreateFileRequest,
-  ): Promise<File> | Observable<File> | File;
+  createFile(request: CreateFileRequest): Promise<File> | Observable<File> | File;
 
-  deleteFile(
-    request: DeleteFileRequest,
-  ): Promise<File> | Observable<File> | File;
+  deleteFile(request: DeleteFileRequest): Promise<File> | Observable<File> | File;
 
-  updateFile(
-    request: UpdateFileRequest,
-  ): Promise<File> | Observable<File> | File;
+  updateFile(request: UpdateFileRequest): Promise<File> | Observable<File> | File;
 }
 
 export function FileDbServiceControllerMethods() {
   return function (constructor: Function) {
-    const grpcMethods: string[] = [
-      'getFile',
-      'createFile',
-      'deleteFile',
-      'updateFile',
-    ];
+    const grpcMethods: string[] = ["getFile", "createFile", "deleteFile", "updateFile"];
     for (const method of grpcMethods) {
-      const descriptor: any = Reflect.getOwnPropertyDescriptor(
-        constructor.prototype,
-        method,
-      );
-      GrpcMethod('FileDbService', method)(
-        constructor.prototype[method],
-        method,
-        descriptor,
-      );
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcMethod("FileDbService", method)(constructor.prototype[method], method, descriptor);
     }
     const grpcStreamMethods: string[] = [];
     for (const method of grpcStreamMethods) {
-      const descriptor: any = Reflect.getOwnPropertyDescriptor(
-        constructor.prototype,
-        method,
-      );
-      GrpcStreamMethod('FileDbService', method)(
-        constructor.prototype[method],
-        method,
-        descriptor,
-      );
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcStreamMethod("FileDbService", method)(constructor.prototype[method], method, descriptor);
     }
   };
 }
 
-export const FILE_DB_SERVICE_NAME = 'FileDbService';
+export const FILE_DB_SERVICE_NAME = "FileDbService";
