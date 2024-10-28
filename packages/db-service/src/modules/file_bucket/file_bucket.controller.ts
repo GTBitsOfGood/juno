b65124b32043bfbd9e512ba125cbef0736f51e3e
Keerth1Keerthi <KeerthiKonuganti@gmail.com>
2024-10-28 11:10:33 -0400

    fix formatting/prettier error

--- packages/db-service/src/modules/file_bucket/file_bucket.controller.ts
+++ packages/db-service/src/modules/file_bucket/file_bucket.controller.ts
@@ -8,78 +8,74 @@ import { status } from '@grpc/grpc-js';
 @Controller()
 @FileBucketProto.BucketBucketDbServiceControllerMethods()
 export class FileBucketController implements BucketBucketDbServiceController {
-    constructor(private readonly fileBucketService: FileBucketService) { }
-    async getBucket(
-        request: FileBucketProto.GetBucketRequest,
-    ): Promise<FileBucketProto.Bucket> {
-        if (!request.name || !request.configId) {
-            throw new RpcException({
-                code: status.INVALID_ARGUMENT,
-                message: 'Both name and configId must be provided',
-            });
-        }
-        const fileBucket = await this.fileBucketService.getBucket(request);
-        if (!fileBucket) {
-            throw new RpcException({
-                code: status.NOT_FOUND,
-                message: 'Bucket not found',
-            });
-        }
-        return fileBucket;
+  constructor(private readonly fileBucketService: FileBucketService) {}
+  async getBucket(
+    request: FileBucketProto.GetBucketRequest,
+  ): Promise<FileBucketProto.Bucket> {
+    if (!request.name || !request.configId) {
+      throw new RpcException({
+        code: status.INVALID_ARGUMENT,
+        message: 'Both name and configId must be provided',
+      });
     }
-    async createBucket(
-        request: FileBucketProto.CreateBucketRequest,
-    ): Promise<FileBucketProto.Bucket> {
-        if (
-            !request.name ||
-            !request.configId ||
-            !request.fileProviderName
-        ) {
-            throw new RpcException({
-                code: status.INVALID_ARGUMENT,
-                message:
-                    'Name, configId, file provider name, files, and metadata must be provided',
-            });
-        }
-        type ResType = {
-            name: string;
-            configId: number;
-            fileProviderName: string;
-            FileServiceFile?: Array<IdentifierProto.FileIdentifier>;  // Include FileServiceFile as optional
-        };
-        //attach foreign keys to config, file provider, and fileservice file when we have access to foreign keys
-        const res: ResType = await this.fileBucketService.createBucket(request);
-        if (!res.FileServiceFile) {
-            res.FileServiceFile = []
-        }
-        const returning: FileBucketProto.Bucket = {
-            name: res.name,
-            configId: res.configId,
-            fileProviderName: res.fileProviderName,
-            FileServiceFile: res.FileServiceFile
-        }
-        return returning;
+    const fileBucket = await this.fileBucketService.getBucket(request);
+    if (!fileBucket) {
+      throw new RpcException({
+        code: status.NOT_FOUND,
+        message: 'Bucket not found',
+      });
     }
-    async deleteBucket(
-        request: FileBucketProto.DeleteBucketRequest,
-    ): Promise<FileBucketProto.Bucket> {
-        if (!request.name || !request.configId) {
-            throw new RpcException({
-                code: status.INVALID_ARGUMENT,
-                message: 'Both name and configId must be provided',
-            });
-        }
-        return this.fileBucketService.deleteBucket(request);
+    return fileBucket;
+  }
+  async createBucket(
+    request: FileBucketProto.CreateBucketRequest,
+  ): Promise<FileBucketProto.Bucket> {
+    if (!request.name || !request.configId || !request.fileProviderName) {
+      throw new RpcException({
+        code: status.INVALID_ARGUMENT,
+        message:
+          'Name, configId, file provider name, files, and metadata must be provided',
+      });
     }
-    async updateBucket(
-        request: FileBucketProto.UpdateBucketRequest,
-    ): Promise<FileBucketProto.Bucket> {
-        if (!request.name || !request.configId || !request.fileProviderName) {
-            throw new RpcException({
-                code: status.INVALID_ARGUMENT,
-                message: 'Name, configId, and updated metadata must be provided',
-            });
-        }
-        return this.fileBucketService.updateBucket(request);
+    type ResType = {
+      name: string;
+      configId: number;
+      fileProviderName: string;
+      FileServiceFile?: Array<IdentifierProto.FileIdentifier>; // Include FileServiceFile as optional
+    };
+    //attach foreign keys to config, file provider, and fileservice file when we have access to foreign keys
+    const res: ResType = await this.fileBucketService.createBucket(request);
+    if (!res.FileServiceFile) {
+      res.FileServiceFile = [];
     }
+    const returning: FileBucketProto.Bucket = {
+      name: res.name,
+      configId: res.configId,
+      fileProviderName: res.fileProviderName,
+      FileServiceFile: res.FileServiceFile,
+    };
+    return returning;
+  }
+  async deleteBucket(
+    request: FileBucketProto.DeleteBucketRequest,
+  ): Promise<FileBucketProto.Bucket> {
+    if (!request.name || !request.configId) {
+      throw new RpcException({
+        code: status.INVALID_ARGUMENT,
+        message: 'Both name and configId must be provided',
+      });
+    }
+    return this.fileBucketService.deleteBucket(request);
+  }
+  async updateBucket(
+    request: FileBucketProto.UpdateBucketRequest,
+  ): Promise<FileBucketProto.Bucket> {
+    if (!request.name || !request.configId || !request.fileProviderName) {
+      throw new RpcException({
+        code: status.INVALID_ARGUMENT,
+        message: 'Name, configId, and updated metadata must be provided',
+      });
+    }
+    return this.fileBucketService.updateBucket(request);
+  }
 }
--- packages/db-service/src/modules/file_bucket/file_bucket.service.ts
+++ packages/db-service/src/modules/file_bucket/file_bucket.service.ts
@@ -5,94 +5,91 @@ import { FileBucketProto, IdentifierProto } from 'juno-proto';
 
 @Injectable()
 export class FileBucketService {
-    constructor(private prisma: PrismaService) { }
-    async getBucket(
-        request: FileBucketProto.GetBucketRequest,
-    ): Promise<FileBucketProto.Bucket> | null {
-        return convertDbBucket(
-            await this.prisma.fileServiceBucket.findUnique({
-                where: {
-                    name_configId: {
-                        name: request.name,
-                        configId: request.configId,
-                    },
-                },
-                include: {
-                    FileServiceFile: true,
-                },
-            }),
-        );
-    }
-    async createBucket(
-        input: FileBucketProto.CreateBucketRequest,
-    ) {
-            const { name, fileProviderName, configId, FileServiceFile } = input
-            return await this.prisma.fileServiceBucket.create({
-                data: {
-                    name,
-                    fileProviderName,
-                    configId,
-                    //add fileservicefile after having access to files
-                    
-                }
-            });
-    }
+  constructor(private prisma: PrismaService) {}
+  async getBucket(
+    request: FileBucketProto.GetBucketRequest,
+  ): Promise<FileBucketProto.Bucket> | null {
+    return convertDbBucket(
+      await this.prisma.fileServiceBucket.findUnique({
+        where: {
+          name_configId: {
+            name: request.name,
+            configId: request.configId,
+          },
+        },
+        include: {
+          FileServiceFile: true,
+        },
+      }),
+    );
+  }
+  async createBucket(input: FileBucketProto.CreateBucketRequest) {
+    const { name, fileProviderName, configId, FileServiceFile } = input;
+    return await this.prisma.fileServiceBucket.create({
+      data: {
+        name,
+        fileProviderName,
+        configId,
+        //add fileservicefile after having access to files
+      },
+    });
+  }
 
-    async updateBucket(
-        request: FileBucketProto.UpdateBucketRequest,
-    ): Promise<FileBucketProto.Bucket> {
-        return convertDbBucket(
-            await this.prisma.fileServiceBucket.update({
-                where: {
-                    name_configId: {
-                        name: request.name,
-                        configId: request.configId,
-                    },
-                },
-                data: {
-                    fileProviderName: request.fileProviderName,
-                },
-                include: {
-                    FileServiceFile: true,
-                },
-            }),
-        );
-    }
+  async updateBucket(
+    request: FileBucketProto.UpdateBucketRequest,
+  ): Promise<FileBucketProto.Bucket> {
+    return convertDbBucket(
+      await this.prisma.fileServiceBucket.update({
+        where: {
+          name_configId: {
+            name: request.name,
+            configId: request.configId,
+          },
+        },
+        data: {
+          fileProviderName: request.fileProviderName,
+        },
+        include: {
+          FileServiceFile: true,
+        },
+      }),
+    );
+  }
 
-    async deleteBucket(
-        request: FileBucketProto.DeleteBucketRequest,
-    ): Promise<FileBucketProto.Bucket> {
-        return convertDbBucket(
-            await this.prisma.fileServiceBucket.delete({
-                where: {
-                    name_configId: {
-                        name: request.name,
-                        configId: request.configId,
-                    },
-                },
-                include: {
-                    FileServiceFile: true,
-                },
-            }),
-        );
-    }
+  async deleteBucket(
+    request: FileBucketProto.DeleteBucketRequest,
+  ): Promise<FileBucketProto.Bucket> {
+    return convertDbBucket(
+      await this.prisma.fileServiceBucket.delete({
+        where: {
+          name_configId: {
+            name: request.name,
+            configId: request.configId,
+          },
+        },
+        include: {
+          FileServiceFile: true,
+        },
+      }),
+    );
+  }
 }
 
 const convertDbBucket = (bucket: any): FileBucketProto.Bucket => {
-    const mappedFiles: IdentifierProto.FileIdentifier[] =
-        bucket.FileServiceFile.map((file) => ({
-            fileId: {
-                path: file.path,
-                configId: file.configId,
-                bucketName: file.bucketName,
-            } as IdentifierProto.FileIdentifier,
-        }));
+  const mappedFiles: IdentifierProto.FileIdentifier[] =
+    bucket.FileServiceFile.map((file) => ({
+      fileId: {
+        path: file.path,
+        configId: file.configId,
+        bucketName: file.bucketName,
+      } as IdentifierProto.FileIdentifier,
+    }));
 
-    const res: FileBucketProto.Bucket = {
-        name: bucket.name,
-        configId: bucket.configId,
-        fileProviderName: bucket.fileProviderName,
-        FileServiceFile: mappedFiles,
-    };
-    return res;
+  const res: FileBucketProto.Bucket = {
+    name: bucket.name,
+    configId: bucket.configId,
+    fileProviderName: bucket.fileProviderName,
+    FileServiceFile: mappedFiles,
+  };
+  return res;
 };
