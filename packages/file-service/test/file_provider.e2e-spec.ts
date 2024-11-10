135e84e3c1cd6a773edbe6b6ec4f5591dc71e321
Syaam Khandaker <syaamkhandaker@gmail.com>
2024-11-10 01:13:50 -0500

    changed null to undefined

--- packages/file-service/test/file_provider.e2e-spec.ts
+++ packages/file-service/test/file_provider.e2e-spec.ts
@@ -91,14 +91,14 @@ describe('File Provider Tests', () => {
         fileProviderClient.registerProvider(
           registerRequest,
           (err: any, resp: any) => {
-            expect(err).not.toBeNull();
+            expect(err).toBeUndefined();
             resolve(resp);
           },
         );
       });
       expect(response).toBeDefined();
     } catch (err) {
-      expect(err).toBeNull();
+      expect(err).toBeUndefined();
     }
   });
 
