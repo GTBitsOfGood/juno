95d80bfcd7a9afde1ed120e96a83abe3a8e0c0a5
Keerth1Keerthi <KeerthiKonuganti@gmail.com>
2024-11-10 08:18:13 -0500

    fixed file request type and aws import

--- packages/file-service/src/modules/file/file.controller.ts
+++ packages/file-service/src/modules/file/file.controller.ts
@@ -3,82 +3,79 @@ import { FileProto } from 'juno-proto';
 import { ClientGrpc } from '@nestjs/microservices';
 import { status } from '@grpc/grpc-js';
 import { RpcException } from '@nestjs/microservices';
-import { GetObjectCommand, S3Client, getSignedUrl } from '@aws-sdk';
-
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 const { FILE_DB_SERVICE_NAME } = FileProto;
 
 @Controller()
 @FileProto.FileServiceControllerMethods()
 export class FileController implements FileProto.FileServiceController {
-  private fileService: FileProto.FileDbServiceClient;
+    private fileService: FileProto.FileDbServiceClient;
 
-  constructor(@Inject(FILE_DB_SERVICE_NAME) private fileClient: ClientGrpc) {}
+    constructor(@Inject(FILE_DB_SERVICE_NAME) private fileClient: ClientGrpc) { }
 
-  onModuleInit() {
-    this.fileService =
-      this.fileClient.getService<FileProto.FileDbServiceClient>(
-        FILE_DB_SERVICE_NAME,
-      );
-  }
-
-  async downloadFile(
-    request: FileProto.DownloadFileRequest,
-  ): Promise<FileProto.DownloadFileResponse> {
-    console.log(request);
-    if (
-      !request ||
-      !request.fileName ||
-      !request.data ||
-      !request.bucket ||
-      !request.provider
-    ) {
-      throw new RpcException({
-        code: status.INVALID_ARGUMENT,
-        message: 'Must provide filename, data, bucket, and provider',
-      });
+    onModuleInit() {
+        this.fileService =
+            this.fileClient.getService<FileProto.FileDbServiceClient>(
+                FILE_DB_SERVICE_NAME,
+            );
     }
 
-    const metadata = request.data;
-    const bucketJSON = JSON.parse(request.bucket);
-    const bucketName = bucketJSON['name'];
-    const configId = bucketJSON['configId'];
-    const fileName = request.fileName;
-    if (configId == undefined || bucketName == undefined) {
-      throw new RpcException({
-        code: status.INVALID_ARGUMENT,
-        message: 'ConfigId and bucketname must be provided',
-      });
+    async downloadFile(
+        request: FileProto.DownloadFileRequest,
+    ): Promise<FileProto.DownloadFileResponse> {
+        console.log(request);
+        if (
+            !request ||
+            !request.fileName ||
+            !request.data ||
+            !request.bucket ||
+            !request.provider
+        ) {
+            throw new RpcException({
+                code: status.INVALID_ARGUMENT,
+                message: 'Must provide filename, data, bucket, and provider',
+            });
+        }
+
+        const metadata = request.data;
+        const bucketJSON = JSON.parse(request.bucket);
+        const bucketName = bucketJSON['name'];
+        const configId = bucketJSON['configId'];
+        const fileName = request.fileName;
+        if (configId == undefined || bucketName == undefined) {
+            throw new RpcException({
+                code: status.INVALID_ARGUMENT,
+                message: 'ConfigId and bucketname must be provided',
+            });
+        }
+
+        //Try connecting to s3 client
+        const client = new S3Client(JSON.parse(request.data));
+
+        //Create File
+        const fileId = {
+            bucketName: bucketName,
+            configId: configId,
+            path: fileName,
+        };
+        const fileRequest = { fileId, metadata }
+        await this.fileService.createFile(fileRequest);
+        //get url
+        const getcommand = new GetObjectCommand({
+            Bucket: bucketName,
+            Key: fileName,
+        });
+        const expiration = 3600;
+        return await getSignedUrl(client, getcommand, expiration);
     }
 
-    //Try connecting to s3 client
-    const client = new S3Client(JSON.parse(request.data));
-
-    //Create File
-    const fileId = {
-      bucketName: bucketName,
-      configId: configId,
-      path: fileName,
-    };
-    await this.fileService.createFile(fileId, metadata);
-    //get url
-    const getcommand = new GetObjectCommand({
-      Bucket: bucketName,
-      Key: fileName,
-    });
-    const expiration = 3600;
-    return await getSignedUrl(client, getcommand, expiration);
-  }
-
-  async uploadFile(
-    request: FileProto.UploadFileRequest,
-  ): Promise<FileProto.UploadFileResponse> {
-    //replace with upload file functionality
-    if (!request) {
-      throw new RpcException({
-        code: status.INVALID_ARGUMENT,
-        message: 'Must provide request information',
-      });
+    async uploadFile(
+        request: FileProto.UploadFileRequest,
+    ): Promise<FileProto.UploadFileResponse> {
+        //replace with upload file functionality
+        throw new RpcException({
+            message: 'Upload File is not implemented',
+        });
     }
-    return null;
-  }
 }
