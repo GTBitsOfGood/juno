da36c172133cb8786950ded02f724a4d402a3aa1
Keerth1Keerthi <KeerthiKonuganti@gmail.com>
2024-11-11 16:55:00 -0500

    fix: import issue

--- packages/file-service/src/app.module.ts
+++ packages/file-service/src/app.module.ts
@@ -3,24 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SentryModule } from '@sentry/nestjs/setup';
 import { HealthModule } from './modules/health/health.module';
-import { FileUploadModule } from './modules/file_upload/file_upload.module';
-import { FileProviderModule } from './modules/file_provider/file_provider.module';
 import { FileModule } from './modules/file/file.module';
 
 @Module({
   imports: [
     SentryModule.forRoot(),
     HealthModule,
-    FileUploadModule,
-    FileProviderModule,
+    FileModule
   ],
-=========
-import { FileModule } from './modules/file/file.module';
-
-@Module({
-  imports: [SentryModule.forRoot(), HealthModule, FileModule],
->>>>>>>>> Temporary merge branch 2
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
