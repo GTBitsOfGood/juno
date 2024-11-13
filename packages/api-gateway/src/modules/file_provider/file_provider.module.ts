ef5b680d1bec8ee503dc5671a82899452c0f58d4
Syaam Khandaker <syaamkhandaker@gmail.com>
2024-11-13 16:54:50 -0500

    added apikeyservice module to file provider api gateway

--- packages/api-gateway/src/modules/file_provider/file_provider.module.ts
+++ packages/api-gateway/src/modules/file_provider/file_provider.module.ts
@@ -10,8 +10,16 @@ import { FileProviderController } from './file_provider.controller';
 import { ApiKeyMiddleware } from 'src/middleware/api_key.middleware';
 
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { FileProviderProto, FileProviderProtoFile } from 'juno-proto';
+import {
+  ApiKeyProtoFile,
+  FileProviderProto,
+  FileProviderProtoFile,
+} from 'juno-proto';
 import { FILE_PROVIDER_FILE_SERVICE_NAME } from 'juno-proto/dist/gen/file_provider';
+import {
+  API_KEY_SERVICE_NAME,
+  JUNO_API_KEY_PACKAGE_NAME,
+} from 'juno-proto/dist/gen/api_key';
 
 // TODO: Make this module Auth protected
 @Module({
@@ -20,6 +28,15 @@ import { FILE_PROVIDER_FILE_SERVICE_NAME } from 'juno-proto/dist/gen/file_provid
       envFilePath: join(__dirname, '../../../../../.env.local'),
     }),
     ClientsModule.register([
+      {
+        name: API_KEY_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: process.env.AUTH_SERVICE_ADDR,
+          package: JUNO_API_KEY_PACKAGE_NAME,
+          protoPath: ApiKeyProtoFile,
+        },
+      },
       {
         name: FILE_PROVIDER_FILE_SERVICE_NAME,
         transport: Transport.GRPC,
