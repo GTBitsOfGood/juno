63b644e388781ae1442791b6b3eb648c5113ebd0
Yolanda Li <142852630+yolanda-y-li@users.noreply.github.com>
2025-03-05 11:53:50 -0500

    Add JWT support to User Auth Middleware (#179)
    Co-authored-by: Yerti <36551149+UZ9@users.noreply.github.com>

--- packages/api-gateway/src/middleware/credentials.middleware.ts
+++ packages/api-gateway/src/middleware/credentials.middleware.ts
@@ -5,57 +5,103 @@ import {
   OnModuleInit,
   HttpException,
   HttpStatus,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { lastValueFrom } from 'rxjs';
 import { ClientGrpc } from '@nestjs/microservices';
-import { CommonProto, UserProto } from 'juno-proto';
+import { CommonProto, UserProto, JwtProto } from 'juno-proto';
 
 const { USER_AUTH_SERVICE_NAME } = UserProto;
+const { JWT_SERVICE_NAME } = JwtProto;
 
 @Injectable()
 export class CredentialsMiddleware implements NestMiddleware, OnModuleInit {
   private userAuthService: UserProto.UserAuthServiceClient;
+  private jwtService: JwtProto.JwtServiceClient;
 
-  constructor(@Inject(USER_AUTH_SERVICE_NAME) private authClient: ClientGrpc) {}
+  constructor(
+    @Inject(USER_AUTH_SERVICE_NAME) private authClient: ClientGrpc,
+    @Inject(JWT_SERVICE_NAME) private jwtClient: ClientGrpc,
+  ) {}
 
   onModuleInit() {
     this.userAuthService =
       this.authClient.getService<UserProto.UserAuthServiceClient>(
         USER_AUTH_SERVICE_NAME,
       );
+    this.jwtService =
+      this.jwtClient.getService<JwtProto.JwtServiceClient>(JWT_SERVICE_NAME);
   }
 
   async use(req: UserReq, res: Response, next: NextFunction) {
     const emailHeader = req.header('X-User-Email');
     const passwordHeader = req.header('X-User-Password');
+    const authHeader = req.header('Authorization');
 
-    // If invalid headers, return 401
-    if (emailHeader === undefined || passwordHeader === undefined) {
-      throw new HttpException(
-        'Invalid credentials provided',
-        HttpStatus.UNAUTHORIZED,
-      );
-    }
+    if (
+      (emailHeader === undefined || emailHeader.length === 0) &&
+      (passwordHeader === undefined || passwordHeader.length === 0)
+    ) {
+      // attempt to check authorization header for jwt when no email or password is provided
+      if (authHeader === undefined || authHeader.length === 0) {
+        throw new UnauthorizedException(
+          'No email, password, or authorization headers',
+        );
+      }
 
-    try {
-      // Wait for RPC result from authentication request
-      const user = await lastValueFrom(
-        this.userAuthService.authenticate({
-          email: emailHeader,
-          password: passwordHeader,
-        }),
-      );
+      const token = this.extractTokenFromHeader(req);
+      if (!token) {
+        throw new UnauthorizedException('JWT not found');
+      }
 
-      req.user = user;
-    } catch {
-      throw new HttpException(
-        'Invalid user credentials',
-        HttpStatus.UNAUTHORIZED,
-      );
+      try {
+        const jwtValidation = this.jwtService.validateUserJwt({
+          jwt: token,
+        });
+        const validationRes = await lastValueFrom(jwtValidation);
+        req.user = validationRes.user;
+      } catch (error) {
+        throw new UnauthorizedException('Invalid user JWT');
+      }
+    } else {
+      // attempt to use email and password
+      if (
+        emailHeader === undefined ||
+        emailHeader.length === 0 ||
+        passwordHeader === undefined ||
+        passwordHeader.length === 0
+      ) {
+        throw new HttpException(
+          'Invalid credentials provided',
+          HttpStatus.UNAUTHORIZED,
+        );
+      }
+
+      try {
+        // Wait for RPC result from authentication request
+        const user = await lastValueFrom(
+          this.userAuthService.authenticate({
+            email: emailHeader,
+            password: passwordHeader,
+          }),
+        );
+
+        req.user = user;
+      } catch {
+        throw new HttpException(
+          'Invalid user credentials',
+          HttpStatus.UNAUTHORIZED,
+        );
+      }
     }
 
     next();
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
 
