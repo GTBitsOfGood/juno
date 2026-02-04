522c313d4656dd229c0ee7f23463d191d9f7d81b
Keshav Worathur <keshavworathur@gmail.com>
2026-02-04 10:45:40 -0500

    fix: unused imports in models

--- packages/api-gateway/src/models/counter.dto.ts
+++ packages/api-gateway/src/models/counter.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsOptional } from 'class-validator';
+import { IsString } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class CounterResponse {
