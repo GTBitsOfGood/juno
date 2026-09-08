360706df85e05b37a6f49c99dcc450cdfd5b2b90
srarkar <rickdsarkar@gmail.com>
2026-09-07 21:14:59 -0400

    feature-flag module file

--- packages/db-service/src/feature-flag/module.ts
+++ packages/db-service/src/feature-flag/module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { PrismaService} from '../prisma.service';
+import { PrismaService } from '../prisma.service';
 import { FeatureFlagController } from './controller';
 import { FeatureFlagService } from './service';
 
 
 @Module({
-  imports: [
-  ],
-  controllers: [],
-  providers: [],
+  imports: [ ],
+  controllers: [ FeatureFlagController ],
+  providers: [ FeatureFlagService, PrismaService ],
 })
 export class FeatureFlagModule {}
