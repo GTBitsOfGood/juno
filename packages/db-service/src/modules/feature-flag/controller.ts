1e87314e55c5b14492c06d2c089bcba5397b976a
srarkar <rickdsarkar@gmail.com>
2026-09-08 14:47:53 -0400

    feature flag controller

--- packages/db-service/src/modules/feature-flag/controller.ts
+++ packages/db-service/src/modules/feature-flag/controller.ts
@@ -4,17 +4,66 @@ import { status } from '@grpc/grpc-js';
 import { Prisma } from '@prisma/client';
 import { FeatureFlagService } from './service';
 import { FeatureFlagProto } from 'juno-proto';
-import { FeatureFlagServiceController } from 'juno-proto/dist/gen/feature-flag';
+import { FeatureFlagServiceController } from 'juno-proto/dist/gen/feature_flag';
 
 @Controller()
-@FeatureFlagProto.FeatureFlagMethods()
-export class FeatureFlagController implements FeatureFlagProto.FeatureFlagController {
+@FeatureFlagProto.FeatureFlagControllerMethods()
+export class FeatureFlagController implements FeatureFlagServiceController {
   constructor(private readonly featureFlagService: FeatureFlagService) {}
   
-  async createFeatureFlag(
-    request: FeatureFlagProto.CreateFeatureFlag,): Promise<FeatureFlagProto.FeatureFlag> {
-      // todo
+  async createFlag(
+    request: FeatureFlagProto.CreateFlagRequest,
+  ): Promise<FeatureFlagProto.FeatureFlag> {
+      if (!request.id || request.id.trim() === '') {
+        throw new RpcException({
+          code: status.INVALID_ARGUMENT,
+          message: 'Provided Flag ID is invalid',
+        });
+    }
+    const featureFlag = await this.featureFlagService.createFlag(request);
+    return featureFlag;
+    }
+
+    async getFlag(
+      request: FeatureFlagProto.GetFlagRequest,
+    ): Promise<FeatureFlagProto.FeatureFlag> {
+      if (!request.id || request.id.trim() === '') {
+        throw new RpcException({
+          code: status.INVALID_ARGUMENT,
+          message: 'Provided Flag ID is invalid',
+        });
+      }
+
+      const featureFlag = await this.featureFlagService.getFlag(request);
+      return featureFlag;
+    }
+
+    async setFlag(
+      request: FeatureFlagProto.SetFlagRequest,
+    ): Promise<FeatureFlagProto.FeatureFlag> {
+      if (!request.id || request.id.trim === '') {
+        throw new RpcException({
+          code: status.INVALID_ARGUMENT,
+          message: 'Provided Flag ID is invalid',
+        });
+      }
+
+      const featureFlag = await this.featureFlagService.setFlag(request);
+      return featureFlag
+    }
+
+    async deleteFlag(
+      request: FeatureFlagProto.DeleteFlagRequest,
+    ): Promise<FeatureFlagProto.DeleteFlagResponse> {
+      if (!request.id || request.id.trim === '') {
+        throw new RpcException({
+          code: status.INVALID_ARGUMENT,
+          message: 'Provided Flag ID is invalid',
+        });
+      }
+      
+      const deleteResponse = await this.featureFlagService.deleteFlag(request);
+      return deleteResponse;
     }
 }
-    
 
