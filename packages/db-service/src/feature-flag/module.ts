7221f7345113c59aa2440b902dd9b88d3c6f8b65
srarkar <rickdsarkar@gmail.com>
2026-09-07 20:51:27 -0400

    begin db-service modules

---
+++ packages/db-service/src/feature-flag/module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { PrismaService} from '../prisma.service';
+import { FeatureFlagController } from './controller';
+import { FeatureFlagService } from './service';
+
+
+@Module({
+  imports: [
+  ],
+  controllers: [],
+  providers: [],
+})
+export class FeatureFlagModule {}
