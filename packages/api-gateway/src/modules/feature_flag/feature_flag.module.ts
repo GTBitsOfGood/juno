2495ad347609bfa003d6753339bcff0666712afc
srarkar <rickdsarkar@gmail.com>
2026-09-09 19:52:14 -0400

    finish feature flag module for api-gateway

--- packages/api-gateway/src/modules/feature_flag/feature_flag.module.ts
+++ packages/api-gateway/src/modules/feature_flag/feature_flag.module.ts
@@ -17,3 +17,45 @@ const { JWT_SERVICE_NAME, JUNO_JWT_PACKAGE_NAME } = JwtProto;
 const { FEATURE_FLAG_SERVICE_NAME, JUNO_FEATURE_FLAG_PACKAGE_NAME } = FeatureFlagProto;
 const { API_KEY_SERVICE_NAME, JUNO_API_KEY_PACKAGE_NAME } = ApiKeyProto;
 
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: join(__dirname, '../../../../../.env.local'),
+    }),
+    ClientsModule.register([
+      {
+        name: JWT_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: process.env.AUTH_SERVICE_ADDR,
+          package: JUNO_JWT_PACKAGE_NAME,
+          protoPath: JwtProtoFile,
+        },
+      },
+      {
+        name: API_KEY_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: process.env.AUTH_SERVICE_ADDR,
+          package: JUNO_API_KEY_PACKAGE_NAME,
+          protoPath: ApiKeyProtoFile,
+        },
+      },
+      {
+        name: FEATURE_FLAG_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: process.env.DB_SERVICE_ADDR,
+          package: JUNO_FEATURE_FLAG_PACKAGE_NAME,
+          protoPath: FeatureFlagProtoFile,
+        },
+      },
+    ]),
+  ],
+  controllers: [FeatureFlagController],
+})
+export class FeatureFlagModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ApiKeyMiddleware).forRoutes('feature-flags/*');
+  }
+}
